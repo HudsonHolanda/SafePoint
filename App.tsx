eb30c7fb29e270a7654926c59009e6034ca6171e
Deyvid098 <Deyvid.Borges773@outlook.com>
2024-09-30 11:56:00 -0300

    Navegação em App.tsx

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LoginScreen from './src/screens/LoginScreen.jsx';
+import SignupScreen from './src/screens/SignupScreen.jsx';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
